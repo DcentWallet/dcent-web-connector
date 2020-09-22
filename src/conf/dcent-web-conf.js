73306d3ecdc0158536db8c8a3d42bc6f4f8386cc
lix82 <kyson@iotrust.kr>
2020-09-22 12:20:45 +0900

    del.test Server ip

--- src/conf/dcent-web-conf.js
+++ src/conf/dcent-web-conf.js
@@ -2,8 +2,7 @@
 
 const config = {
   timeOutMs: 60000,
-  // popUpUrl: 'https://bridge.dcentwallet.com/v2'
-  popUpUrl: 'http://localhost:9000/v2'
+  popUpUrl: 'https://bridge.dcentwallet.com/v2'
 }
 
 module.exports = {

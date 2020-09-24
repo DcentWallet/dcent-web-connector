728994cd69f3397d7134dc782a157fc5638a3000
KyungeunKim <ke.kim@iotrust.kr>
2020-09-24 15:55:31 +0900

    bridge test 변경

--- tests/unit/1_bridge_test/6_coin.sign.message.spec.js
+++ tests/unit/1_bridge_test/6_coin.sign.message.spec.js
@@ -6,6 +6,30 @@ const puppeteer = require('puppeteer')
 /* //////////////////////////////////////////////////////////////////////// */
 /* */
 /* //////////////////////////////////////////////////////////////////////// */
+const utils = require('caver-js/packages/caver-utils')
+const AccountLib = require('eth-lib/lib/account')
+const Hash = require('eth-lib/lib/hash')
+
+function recover (message, signature, preFixed) {
+
+    if (!preFixed) {
+        message = hashMessage(message)
+    }
+
+    return AccountLib.recover(message, signature)
+}
+
+function hashMessage (data) {
+    const message = utils.isHexStrict(data) ? utils.hexToBytes(data) : data
+    const messageBuffer = Buffer.from(message)
+    const preamble = `\x19Klaytn Signed Message:\n${message.length}`
+    const preambleBuffer = Buffer.from(preamble)
+    // klayMessage is concatenated buffer (preambleBuffer + messageBuffer)
+    const klayMessage = Buffer.concat([preambleBuffer, messageBuffer])
+    // Finally, run keccak256 on klayMessage.
+    return Hash.keccak256(klayMessage)
+}
+
 
 describe('[dcent-web-connector] Bridge - init', () => {
     let bowser
@@ -41,6 +65,21 @@ describe('[dcent-web-connector] Bridge - init', () => {
         done()
     })
 
+    // it('getSignedMessage() - current not support', async (done) => {
+      
+    //     const coinType = DcentWebConnector.coinType.KLAYTN
+    //     const key = "m/44'/8217'/0'/0/0"
+    //     const message = 'This is a message!!'
+
+    //     var response = await page.evaluate((coinType, key, message) => {
+    //         // eslint-disable-next-line no-undef
+    //         return getSignedMessage(coinType, key, message)
+    //     }, coinType, key, message)
+
+    //     expect(response.header.status).toBe(Values.RESP_STATUS.ERROR)
+    //     done()
+    // })
+
     it('getSignedMessage() - current not support', async (done) => {
       
         const coinType = DcentWebConnector.coinType.KLAYTN
@@ -52,7 +91,15 @@ describe('[dcent-web-connector] Bridge - init', () => {
             return getSignedMessage(coinType, key, message)
         }, coinType, key, message)
 
-        expect(response.header.status).toBe(Values.RESP_STATUS.ERROR)
+        expect(response.header.status).toBe(Values.RESP_STATUS.SUCCESS)
+        expect(response.body.parameter.address).toBeDefined()
+        expect(response.body.parameter.sign).toBeDefined()
+
+        // value check 
+        const sign = response.body.parameter.sign
+        const addr = recover(message, sign)
+        expect(addr).toBe(response.body.parameter.address)
+
         done()
     })
 

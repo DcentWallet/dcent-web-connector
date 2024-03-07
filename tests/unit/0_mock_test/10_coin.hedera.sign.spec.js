3820987c775b4617477983a470677a380e3eca20
AnHyoChul <hyochul.an@iotrust.kr>
2024-03-07 18:25:04 +0900

    #107 Updating to support Node.js v18.x

--- tests/unit/0_mock_test/10_coin.hedera.sign.spec.js
+++ tests/unit/0_mock_test/10_coin.hedera.sign.spec.js
@@ -53,7 +53,6 @@ describe('[dcent-web-connector] MOCK - coin sign', () => {
         DcentWebConnector.popupWindowClose()
     })
 
-   
     it('getHederaSignedTransaction() -sucess ', async (done) => {
         var response
         try {
@@ -63,7 +62,7 @@ describe('[dcent-web-connector] MOCK - coin sign', () => {
                 symbol: 'HBAR',
                 decimals: '8',
             }
-                       
+
             response = await DcentWebConnector.getHederaSignedTransaction(transactionJson)
         } catch (e) {
             response = e
@@ -82,7 +81,7 @@ describe('[dcent-web-connector] MOCK - coin sign', () => {
                 symbol: 'JAM',
                 decimals: '8',
             }
-                       
+
             response = await DcentWebConnector.getHederaSignedTransaction(transactionJson)
         } catch (e) {
             response = e
@@ -99,7 +98,7 @@ describe('[dcent-web-connector] MOCK - coin sign', () => {
                 unsignedMsg: '19486564657261205369676e6564204d6573736167653a0a333654686973206973206865646572615f7369676e4d6573736167652773206d657373616765',
                 path: `m/44'/3030'/0'`,
             }
-                       
+
             response = await DcentWebConnector.getHederaSignedMessage(transactionJson)
         } catch (e) {
             response = e
